c54c6544a3027a4fa21f774cd160b66b1262dc67
Michael Chang <mbchang@mit.edu>
2016-04-16 11:53:07 -0400

    change random seed

--- dqn/NeuralQLearner.lua
+++ dqn/NeuralQLearner.lua
@@ -58,8 +58,8 @@ function nql:__init(args)
     self.gpu            = args.gpu
 
     self.ncols          = args.ncols or 1  -- number of color channels in input
-    -- self.input_dims     = args.input_dims or {self.hist_len*self.ncols, 84, 84}  -- this incorporates hist_len!
-    self.input_dims     = args.input_dims or {self.hist_len,3, 210, 160}  -- this incorporates hist_len!
+    self.input_dims     = args.input_dims or {self.hist_len*self.ncols, 84, 84}  -- this incorporates hist_len!
+    -- self.input_dims     = args.input_dims or {self.hist_len,3, 210, 160}  -- this incorporates hist_len!  -- TODO
     self.preproc        = args.preproc  -- name of preprocessing network
     self.histType       = args.histType or "linear"  -- history type to use
     self.histSpacing    = args.histSpacing or 1
