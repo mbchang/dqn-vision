d79cc6c0d8f177834132dae1cb2bf1bc0ca5f0ae
Michael Chang <mbchang@mit.edu>
2016-04-20 13:33:21 -0400

    updated args into predictive_udcign_atari3

--- dqn/predictive_udcign_atari3.lua
+++ dqn/predictive_udcign_atari3.lua
@@ -4,11 +4,11 @@ local D2 = require 'DownsampledDecoder2'
 function load_pred_net(args)
     local dim_hidden = 200
     local color_channels = 1
-    local feature_maps = 4
+    local feature_maps = 72
     local noise = 0.1
     local sharpening_rate = 10
-    local scheduler_iteration = torch.zeros(1)  -- TODO! scheduler_iteration should be a field in NeuralQPredictiveLearner
-    local num_heads = 1
+    -- local scheduler_iteration = torch.zeros(1)  -- TODO! scheduler_iteration should be a field in NeuralQPredictiveLearner
+    local num_heads = 3
 
     local enc2 = E2(dim_hidden, color_channels, feature_maps)
     local dec2 = D2(dim_hidden, color_channels, feature_maps, noise, sharpening_rate, args.scheduler_iteration, num_heads)
