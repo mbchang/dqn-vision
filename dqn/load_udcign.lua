8a25dfc184df3e4ca4ac92aace4daf8bcc810c9b
mbchang <mbchang@users.noreply.github.com>
2016-04-25 13:45:57 -0400

    NeuralQLearnerReshape works
    for load_udcign's network, you reshape the input before you input into the encoder, then you reshape the encoder output before you put it back into the decoder

--- dqn/load_udcign.lua
+++ dqn/load_udcign.lua
@@ -3,22 +3,21 @@
 require "initenv"
 local optnet = require 'optnet'
 
-function load_udcign_untrained(encoder, args)
+function load_encoder(encoder, args)
     local encoder_dim = args.hist_len*args.p_dim_hidden
 
     local net = nn.Sequential()
 
     -- encoder
     local pre_encoder = nn.Sequential()
-    pre_encoder:add(nn.Reshape(args.minibatch_size*args.hist_len,1,84,84))
     pre_encoder:add(encoder) -- output is (bsize*hist_len, 200)
     net:add(pre_encoder)
 
+    -- note that you have to reshape the input when you do fwd and bwd!
+
     -- add the last fully connected layer (to actions)
     local linear = nn.Sequential()
-    linear:add(nn.Reshape(args.minibatch_size,encoder_dim))
     linear:add(nn.Linear(encoder_dim, args.n_actions))
-
     net:add(linear)
 
     if args.gpu >=0 then
