f52e7e8d98606b9dd90ff66d5791bb79dc837849
Will Whitney <wwhitney@mit.edu>
2016-04-27 12:40:10 -0400

    setting up for first two experiments with pretrained nets and fixed weights

--- dqn/udcign_vanilla_atari3.lua
+++ dqn/udcign_vanilla_atari3.lua
@@ -4,10 +4,7 @@ return function(args)
     --                 'atari_motion_scale_3_noise_0.1_heads_3_sharpening_rate_10_'..
     --                 'gpu_learning_rate_0.0002_dataset_name_breakout_'..
     --                 'frame_interval_3/epoch28.12_0.2033.t7'
-    local cp_path = '/home/mbchang/code/unsupervised-dcign/logslink/'..
-                    'atari_vanilla_motion_scale_3_noise_0.1_heads_3_'..
-                    'sharpening_rate_10_gpu_learning_rate_0.0002_'..
-                    'dataset_name_breakout_frame_interval_3/'
+    local cp_path = '/om/user/wwhitney/unsupervised-dcign/networks/down_motion_scale_3_noise_0.1_heads_3_sharpening_rate_10_gpu_learning_rate_0.0001_model_autoencoder_dataset_name_space_invaders_frame_interval_1/epoch50.00_0.1298.t7'
     args.vanilla = true
     return load_udcign_encoder(cp_path, args)
 end
